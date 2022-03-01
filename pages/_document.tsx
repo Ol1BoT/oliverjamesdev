62508b134d084de14a900b65bc8dd610e52c39bc
Ollie Mignot <olliemignot@gmail.com>
2022-03-01 20:07:43 +1100

    add meta tags for twitter and OG

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,18 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content="@OllieMignot" />
+          <meta property="og:url" content="https://oliverjames.dev" />
+          <meta property="og:title" content="Oliver Mignot Person Website" />
+
+          <meta
+            property="og:description"
+            content="Personal site detailing my work, projects and products."
+          />
+          <meta property="og:image" content="./oj_light.png" />
+        </Head>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
